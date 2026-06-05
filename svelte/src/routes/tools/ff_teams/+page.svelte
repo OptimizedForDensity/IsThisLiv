97a28176903c4f2ccf75336a7d8702da60e0a834
OptimizedForDensity <105244635+OptimizedForDensity@users.noreply.github.com>
2026-06-05 19:06:31 -0400

    Fix fantasy football password resets

--- svelte/src/routes/tools/ff_teams/+page.svelte
+++ svelte/src/routes/tools/ff_teams/+page.svelte
@@ -1,43 +1,40 @@
 <script lang="ts">
-	import { api } from '$lib/helper';
-	import Modal from '$lib/modal.svelte';
-	export let data: Promise<any>;
-	let newUser = '';
-	let newPassword = '';
-	async function resetPassword(id: string) {
-		newPassword = (await api(fetch, '/ff/resetPassword', { id })).prv;
-	}
+    import { api } from '$lib/helper';
+    import Modal from '$lib/modal.svelte';
+
+    export let data: { teamList: any[] };
+
+    let newPassword = '';
+
+    async function resetPassword(id: string) {
+        newPassword = (await api(fetch, '/ff/resetPassword', { id })).prv;
+    }
 </script>
 
 <container>
-	{#if newPassword}
-		<Modal
-			close={() => {
-				newPassword = '';
-			}}
-			title={'Password'}
-		>
-			Team's new password is {newPassword}
-		</Modal>
-	{/if}
-	{#await data then users}
-		<table>
-			<tbody>
-			{#each users as user}
-				<tr>
-					<td>{user.name}</td>
-					<td
-						><button
-							on:click={() => {
-								resetPassword(user.teamID);
-							}}>Reset Password</button
-						></td
-					>
-				</tr>
-			{/each}
-			</tbody>
-		</table>
-	{/await}
+    {#if newPassword}
+        <Modal
+            close={() => { newPassword = ''; }}
+            title={'Password'}
+        >
+            Team's new password is {newPassword}
+        </Modal>
+    {/if}
+
+    <table>
+        <tbody>
+            {#each data.teamList as user}
+                <tr>
+                    <td>{user.name}</td>
+                    <td>
+                        <button on:click={() => resetPassword(user.teamID)}>
+                            Reset Password
+                        </button>
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
 </container>
 
 <style>
